6941e0f73b18bd6fb784dc6a591d9403436b7eaf
SahilShameerDev <smsahilms346@protonmail.com>
2023-02-11 21:29:09 +0530

    minimax algorithm added

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-
 board = ['']*10
 computer = 'X'
 human = 'O'
@@ -15,21 +13,22 @@ def display_board(board):
 # checks if the cells are filled
 
 
-def is_vertically_filled():
-    return board[1] == board[2] == board[3] and board[1] != '' or board[4] == board[5] == board[6] and board[4] != '' or board[7] == board[8] == board[9] and board[7] != ''
+def is_vertically_filled(letter):
+    return board[1] == board[2] == board[3] and board[1] != letter or board[4] == board[5] == board[6] and board[4] != letter or board[7] == board[8] == board[9] and board[7] != letter
 
 
-def is_horizontally_filled():
-    return board[1] == board[4] == board[7] and board[1] != '' or board[2] == board[5] == board[8] and board[2] != '' or board[3] == board[6] == board[9] and board[3] != ''
+def is_horizontally_filled(letter):
+    return board[1] == board[4] == board[7] and board[1] != letter or board[2] == board[5] == board[8] and board[2] != letter or board[3] == board[6] == board[9] and board[3] != letter
 
 
-def is_diaganolly_filled():
-    return board[1] == board[5] == board[9] and board[1] != '' or board[3] == board[5] == board[7] and board[3] != ''
+def is_diaganolly_filled(letter):
+    return board[1] == board[5] == board[9] and board[1] != letter or board[3] == board[5] == board[7] and board[3] != letter
 
 
 # checks if the game has ended
 def check_win():
-    if is_horizontally_filled() or is_diaganolly_filled() or is_vertically_filled():
+    letter = ''
+    if is_horizontally_filled(letter) or is_diaganolly_filled(letter) or is_vertically_filled(letter):
         return True
 
 
@@ -56,17 +55,60 @@ def insert(letter, pos):
             print('draw')
             exit()
     else:
-        if letter == 'O':
-            pos = int(input('Not a free space pls re-enter a position: '))
-        elif letter == 'X':
-            pos = random.randint(1, 9)
-
+        pos = int(input('Not a free space pls re-enter a position: '))
         insert(letter, pos)
 
 
+def is_win(letter):
+    if is_horizontally_filled(letter) or is_diaganolly_filled(letter) or is_vertically_filled(letter):
+        return True
+
+
+def minimax(board, is_maximizing):
+    if is_win(computer):
+        return 1
+    elif is_win(human):
+        return -1
+    elif check_draw():
+        return 0
+
+    if is_maximizing:
+        best_score = -2
+        for i in range(1, len(board)):
+            if is_available(i):
+                board[i] = computer
+                score = minimax(board, False)
+                board[i] = ''
+                best_score = max(best_score, score)
+
+        return best_score
+    else:
+        best_score = 2
+        for i in range(1, len(board)):
+            if is_available(i):
+                board[i] = human
+                score = minimax(board, True)
+                board[i] = ''
+                best_score = min(best_score, score)
+
+        return best_score
+
+
 def computer_move(letter):
-    pos = random.randint(1, 9)
-    insert(letter, pos)
+    best_score = -2
+    best_pos = 0
+
+    for i in range(1, len(board)):
+        if is_available(i):
+            board[i] = letter
+            score = minimax(board, False)
+            board[i] = ''
+            if score > best_score:
+                best_score = score
+                best_pos = i
+
+    insert(letter, best_pos)
+    return
 
 
 def human_move(letter):
